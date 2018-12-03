26d38cd1916104b635e7c677aa03d1b007c8ed7c
canthny <canthnyq8@126.com>
2018-12-03 23:55:45 +0800

    springframework test commit

---
+++ tanghao-learning-test/src/test/java/com/canthny/spring/framework/learning/test/XmlBeanFactoryTest.java
@@ -0,0 +1,25 @@
+package com.canthny.spring.framework.learning.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tanghao.learning.test.spring.framework.service.TestServiceA;
+
+/**
+ * @Author： Canthny
+ * @Description： XmlBeanFactory学习
+ * @Date： Created in 2018/12/3 19:55
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class XmlBeanFactoryTest {
+
+    @Test
+    public void test1(){
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("testXmlBeanFactory.xml"));
+        TestServiceA testService = (TestServiceA) beanFactory.getBean("testServiceA");
+        testService.test();
+    }
+}
